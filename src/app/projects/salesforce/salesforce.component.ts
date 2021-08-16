8832b23bcc10df1d2206157c9f0d853c551c230b
ZoeCharbonnier <zoe.charbonnier@hotmail.com>
2021-08-16 10:37:14 +0200

    add appstate

--- src/app/projects/salesforce/salesforce.component.ts
+++ src/app/projects/salesforce/salesforce.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/shared/app.state';
 
 @Component({
   selector: 'app-salesforce',
   templateUrl: './salesforce.component.html',
-  styleUrls: ['./salesforce.component.scss']
+  styleUrls: ['./salesforce.component.scss'],
 })
 export class SalesforceComponent implements OnInit {
-
-  constructor() { }
+  constructor(public appState: AppState) {}
 
   ngOnInit(): void {
+    this.appState.header$.next('Salesforce');
+    this.appState.subheader$.next('');
   }
-
 }
